db71e2d71a2177a92352e8595f276c50e3ed9c54
Alumno <Alumno@PC25>
2019-10-24 12:06:12 -0500

    chipres

---
+++ vectores/vectores/vectores.cpp
@@ -0,0 +1,20 @@
+// vectores.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+void suma();
+
+int main()
+{
+	suma();
+	return 0;
+}
+void suma()
+{
+	int a, b, suma;
+	cout << "Dame el primero: ";
+	cin >> a;
+	suma = a + b;
+}
